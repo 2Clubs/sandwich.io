323c7b358b3d732da95e989515a8fb21585692e3
zanetowell <zanetowell@gmail.com>
2023-04-28 16:01:28 -0400

    adds sandwich and ingredient models and displays home page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
-
+from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
+
+TEMP = (
+    ('H', 'Hot'),
+    ('C', 'Cold'),
+)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'This is {self.name}'
+    
+    def get_absolute_url(self):
+        return reverse("ingredients_detail", kwargs={"pk": self.pk})
+
+
+class Sandwich(models.Model):
+    name = models.CharField(max_length=50)
+    temp = models.CharField(
+        max_length=1,
+        choices=TEMP,
+        default=TEMP[0][1]
+    )
+    description = models.TextField(max_length=100)
+    ingredients = models.ManyToManyField(Ingredient)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
+    
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"pk": self.pk})
+    
+
+
+
+
+
+
+
