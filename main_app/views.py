5ae8437b3ff9e38d5226442f3016b3ceb6d092d0
zanetowell <zanetowell@gmail.com>
2023-04-28 16:55:53 -0400

    adds sign up and login functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from .models import Sandwich, Ingredient
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    
+    return render(request,'home.html')
+    
 
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('/')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
