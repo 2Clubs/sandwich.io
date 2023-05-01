733bef4b41580d729d11351c0e28a2ea5e20ab2e
2clubs <bennettdan85@gmail.com>
2023-05-01 14:09:49 -0400

    adds ingredients path

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('ingredients/', views.IngredientsIndex.as_view(), name='ingredients_index')
 ]
