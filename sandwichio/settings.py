dddd08f6f329640f10c5cf5924dcd77e458e411e
zanetowell <zanetowell@gmail.com>
2023-05-01 16:15:52 -0400

    moves user from settings to hidden

--- sandwichio/settings.py
+++ sandwichio/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': '2Clubs/sandwich-io',
-        'USER': 'zanetowell',
+        'USER': str(os.getenv('USERNAME')),
         'PASSWORD': str(os.getenv('PASSWORD')),
         'HOST': 'db.bit.io',
         'PORT': '5432',
